f965c7435e226322d78a529403cb499b5c389047
minhnguyen28204 <125328581+MinhNguyen282@users.noreply.github.com>
2025-07-09 10:29:57 +0700

    Fix: Add auto scroll to top when navigate to other page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -12,9 +12,19 @@ import ProjectDetails from './components/ProjectDetails';
 import CV from './components/CV';
 import Experience from "./components/Experience"
 
+// Scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
